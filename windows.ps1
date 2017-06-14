8eb0a1e2aa71ffbd09b90c1018153df98b4960a8
Ghust1995 <gustavo.ceci95.com>
2017-06-13 21:47:47 -0300

    Fixed powershell script

--- windows.ps1
+++ windows.ps1
@@ -1,9 +1,10 @@
-try { cmd /c mklink ~\_vimrc .\_vimrc }
-catch { "Link already exists" }
+$path = ($pwd).path + "\_vimrc"
+Try { cmd /c mklink C:\Users\gusta\_vimrc $path}
+Catch { "Link already exists" }
 
 
-try { md ~\vimfiles\autoload }
-catch { "Autoload folder already exists" }
+Try { md ~\vimfiles\autoload }
+Catch { "Autoload folder already exists" }
 
 $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile($uri, $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("~\vimfiles\autoload\plug.vim"))
